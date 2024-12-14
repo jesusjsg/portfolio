4e116a9c472d5bbf254458ed3f646d8ab8ec4d53
jesus <jesusjsg@proton.me>
2024-12-14 07:24:38 -0400

    feat: new portfolio with tailwind

---
+++ src/assets/js/main.js
@@ -0,0 +1,27 @@
+import Typed from 'typed.js'
+import '../css/styles.css'
+import '../js/components/themeColor.js'
+import 'preline'
+
+const textTyped = document.querySelector('#typed-words')
+const currentYear = document.querySelector('#current-year')
+
+function main() {
+    typedText()
+    getCurrentYear()
+}
+
+function typedText() {
+    new Typed(textTyped, {
+        strings: ['desarrollador backend junior', 'freelancer', 'ingeniero de sistemas'],
+        typeSpeed: 90,
+        loop: true,
+    })
+}
+
+function getCurrentYear() {
+    const year = new Date().getFullYear()
+    currentYear.textContent = year
+}
+
+document.addEventListener('DOMContentLoaded', main)

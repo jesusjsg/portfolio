8738066f282877789c9e960dd5b865ab10115357
jesus <jesusjsgdev@gmail.com>
2025-11-16 20:29:53 -0400

    feat(ui): update card and social button components

--- src/components/ui/SocialButton.jsx
+++ src/components/ui/SocialButton.jsx
@@ -3,7 +3,7 @@ export const SocialButton = ({ text, icon, href }) => {
     <a
       href={href}
       target="_blank"
-      className="py-1 px-3 inline-flex items-center rounded-full gap-x-2 text-sm font-medium border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+      className="py-1 px-3 inline-flex items-center rounded-full gap-x-2 text-sm font-medium border border-transparent text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:hover:bg-neutral-800 dark:focus:bg-neutral-700"
     >
       {text}
       {icon}
